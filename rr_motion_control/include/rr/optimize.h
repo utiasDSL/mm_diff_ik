2919485132b36c46819ae7e2bded2dde74255fd9
Adam Heins <mail@adamheins.com>
2019-07-25 17:30:17 -0400

    Implement basic opt problem.

--- rr_motion_control/include/rr/optimize.h
+++ rr_motion_control/include/rr/optimize.h
@@ -9,10 +9,41 @@
 #include <sensor_msgs/JointState.h>
 
 #include "rr/rr.h"
+#include "rr/kinematics.h"
+
+
+// QuadProgDense(int nrvar, int nreq, int nrineq);
+//
+// bool solve(const MatrixXd& Q, const VectorXd& C,
+// 	const MatrixXd& Aeq, const VectorXd& Beq,
+// 	const MatrixXd& Aineq, const VectorXd& Bineq,
+// 	bool isDecomp=false);
+//
 
 
 namespace rr {
     class IKOptimizer {
-        // TODO optimize the hell out of the joint velocity
+        public:
+            IKOptimizer() {};
+
+            bool solve(QVector& q, Vector6d& ee_vel, QVector& dq_opt) {
+                // Objective
+                QMatrix Q = QMatrix::Identity();
+                QVector C = QVector::Zero();
+
+                // Constraints
+                JacMatrix J;
+                Kinematics::jacobian(q, J);
+
+                // No inequality constraints for now.
+                Eigen::Matrix<double, 1, 0> Aineq, Bineq;
+
+                // Solve the QP.
+                Eigen::QuadProgDense qp(NUM_JOINTS, 6, 0);
+                bool ret = qp.solve(Q, C, J, ee_vel, Aineq, Bineq);
+                dq_opt = qp.result();
+
+                return ret;
+            }
     };
 }
