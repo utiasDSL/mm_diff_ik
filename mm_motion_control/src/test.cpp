a70aa285e121523409f56bbb3042e60cbbddde58
Adam Heins <mail@adamheins.com>
2019-07-31 10:29:44 -0400

    Remove Jacobian test since it's an actual gtest now.

--- mm_motion_control/src/test.cpp
+++ mm_motion_control/src/test.cpp
@@ -1,12 +1,18 @@
 #include <Eigen/Eigen>
 #include <iostream>
 
+#include "mm/mm.h"
 #include "mm/kinematics.h"
 #include "mm/interp.h"
+#include "mm/optimize.h"
 
 using namespace Eigen;
 
 typedef Matrix<double, 1, 1> Vector1d;
+
+// TODO
+// 1. move these tests to actual gtest tests
+// 2. make them better
 
 void test_interp() {
     Vector1d x1; x1 << 4;
@@ -24,7 +30,23 @@ void test_interp() {
     std::cout << x << " " << dx << std::endl;
 }
 
+void test_qp() {
+    mm::QVector q = mm::QVector::Zero();
+    mm::Vector6d ee_vel;
+    ee_vel << 1, 0, 0, 0, 0, 0;
+
+    mm::QVector dq_opt;
+
+    mm::IKOptimizer optimizer;
+    bool ret = optimizer.solve(q, ee_vel, dq_opt);
+
+    ROS_INFO_STREAM("dq_opt = " << dq_opt);
+    ROS_INFO_STREAM("ret = " << ret);
+}
+
+
 int main() {
-    test_interp();
+    // test_qp();
+    // test_interp();
     return 0;
 }
