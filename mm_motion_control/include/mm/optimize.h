9fa804c03098bdf5f868438c3ea6ea9c5749bbc8
Adam Heins <mail@adamheins.com>
2019-08-12 14:38:49 -0400

    Implement linearized manipulability.

--- mm_motion_control/include/mm/optimize.h
+++ mm_motion_control/include/mm/optimize.h
@@ -24,59 +24,114 @@
 
 
 namespace mm {
-    class IKOptimizer {
-        public:
-            IKOptimizer() {};
 
-            // Create and solve the QP.
-            // q: Current value of joint angles.
-            // ee_vel: Desired task space velocity of end effector.
-            // dq_opt: Optimal values of joint velocities.
-            // Returns true if optimization problem was solved successfully
-            // (i.e. constraints satisified).
-            bool solve(const JointVector& q, const Vector6d& ee_vel,
-                       JointVector& dq_opt) {
-                // Objective
-                JointMatrix Q = JointMatrix::Identity();
-                JointVector C = JointVector::Zero();
+// Approximate gradient using central differences.
+// f: Function mapping N-dim vector input to scalar output
+// h: Step size
+// x: Input point
+// grad: The gradient, populated by this function.
+template<int N>
+void approx_gradient(double (*f)(const Eigen::Matrix<double, N, 1>&),
+                     double h, const Eigen::Matrix<double, N, 1>& x,
+                     Eigen::Matrix<double, N, 1>& grad) {
+    typedef Eigen::Matrix<double, N, N> MatrixNd;
+    typedef Eigen::Matrix<double, N, 1> VectorNd;
 
-                // Equality constraints
-                JacobianMatrix J;
-                Kinematics::jacobian(q, J);
+    MatrixNd I = MatrixNd::Identity();
 
-                Eigen::Matrix<double, 6, 9> Aeq = J;
-                Eigen::Matrix<double, 6, 1> Beq = ee_vel;
-
-                // Inequality constraints
-                // Eigen::Matrix<double, 1, 0> Aineq, Bineq;
-
-                // Requires -1 <= dq <= 1
-                Eigen::Matrix<double, 18, 9> Aineq;
-                Aineq << JointMatrix::Identity(), -JointMatrix::Identity();
-                Eigen::Matrix<double, 18, 1> Bineq = Eigen::Matrix<double, 18, 1>::Ones();
-
-                // Solve the QP.
-                Eigen::QuadProgDense qp(NUM_JOINTS, 6, 18);
-                bool success = qp.solve(Q, C, Aeq, Beq, Aineq, Bineq);
-                dq_opt = qp.result();
-
-                return success;
-            }
-
-        private:
-            // Linearize manipulability around joint values q, returning the
-            // value m, gradient dm, and Hessian Hm.
-            void linearized_manipulability(const JointVector& q, double& m,
-                                           JointVector& dm, JointMatrix& Hm) {
-                double h = 1e-8; // Step size
-
-                m = Kinematics::manipulability(q);
-
-                // Numerical gradient using central difference.
-                JointMatrix E = JointMatrix::Identity();
-                for (int i = 0; i < NUM_JOINTS; ++i) {
-                    dm(i) = (Kinematics::manipulability(q + h*E.col(i)) - Kinematics::manipulability(q - h*E.col(i))) / (2*h);
-                }
-            }
-    };
+    for (int i = 0; i < N; ++i) {
+        VectorNd Ii = I.col(i);
+        grad(i) = (f(x + h*Ii) - f(x - h*Ii)) / (2*h);
+    }
 }
+
+
+// Approximate Hessian using central differences.
+// f: Function mapping N-dim vector input to scalar output
+// h: Step size
+// x: Input point
+// H: The Hessian, populated by this function.
+template<int N>
+void approx_hessian(double (*f)(const Eigen::Matrix<double, N, 1>&),
+                    double h, const Eigen::Matrix<double, N, 1>& x,
+                    Eigen::Matrix<double, N, N>& H) {
+    typedef Eigen::Matrix<double, N, N> MatrixNd;
+    typedef Eigen::Matrix<double, N, 1> VectorNd;
+
+    MatrixNd I = MatrixNd::Identity();
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            VectorNd Ii = I.col(i);
+            VectorNd Ij = I.col(j);
+            H(i,j) = (f(x + h*Ii + h*Ij) - f(x + h*Ii - h*Ij)
+                    - f(x - h*Ii + h*Ij) + f(x - h*Ii - h*Ij)) / (4*h*h);
+        }
+    }
+}
+
+class IKOptimizer {
+    public:
+        IKOptimizer() {};
+
+        // Create and solve the QP.
+        // q: Current value of joint angles.
+        // ee_vel: Desired task space velocity of end effector.
+        // dq_opt: Optimal values of joint velocities.
+        // Returns true if optimization problem was solved successfully
+        // (i.e. constraints satisified).
+        // TODO we're going to want dt here
+        bool solve(const JointVector& q, const Vector6d& ee_vel,
+                   JointVector& dq_opt) {
+            // Minimize velocity objective.
+            JointMatrix Q = JointMatrix::Identity();
+            JointVector C = JointVector::Zero();
+
+            // Manipulability objective.
+            // JointVector dm;
+            // JointMatrix Hm;
+            // linearized_manipulability(q, dm, Hm);
+
+            // Equality constraints: track the reference velocity.
+            // TODO relaxation
+            JacobianMatrix J;
+            Kinematics::jacobian(q, J);
+
+            Eigen::Matrix<double, 6, 9> Aeq = J;
+            Eigen::Matrix<double, 6, 1> Beq = ee_vel;
+
+            // Inequality constraints
+            // Eigen::Matrix<double, 1, 0> Aineq, Bineq;
+
+            // Requires -1 <= dq <= 1
+            Eigen::Matrix<double, 18, 9> Aineq;
+            Aineq << JointMatrix::Identity(), -JointMatrix::Identity();
+            Eigen::Matrix<double, 18, 1> Bineq = Eigen::Matrix<double, 18, 1>::Ones();
+
+            // Solve the QP.
+            Eigen::QuadProgDense qp(NUM_JOINTS, 6, 18);
+            bool success = qp.solve(Q, C, Aeq, Beq, Aineq, Bineq);
+            dq_opt = qp.result();
+
+            return success;
+        }
+
+    private:
+        // Linearize manipulability around joint values q, returning the
+        // value m, gradient dm, and Hessian Hm.
+        void linearized_manipulability(const JointVector& q, JointVector& dm,
+                                       JointMatrix& Hm) {
+            double h = 1e-8; // Step size
+
+            approx_gradient<NUM_JOINTS>(&Kinematics::manipulability, h, q, dm);
+            approx_hessian<NUM_JOINTS>(&Kinematics::manipulability, h, q, Hm);
+
+            // Numerical gradient using central difference.
+            // JointMatrix E = JointMatrix::Identity();
+            // for (int i = 0; i < NUM_JOINTS; ++i) {
+            //     dm(i) = (Kinematics::manipulability(q + h*E.col(i)) - Kinematics::manipulability(q - h*E.col(i))) / (2*h);
+            // }
+        }
+}; // class IKOptimizer
+
+} // namespace mm
