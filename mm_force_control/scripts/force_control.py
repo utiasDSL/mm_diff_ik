4b979190fed0d57420154e90c27261a8663c4ebc
Adam Heins <mail@adamheins.com>
2019-09-02 12:01:27 -0400

    Clean up the force controller.

--- mm_force_control/scripts/force_control.py
+++ mm_force_control/scripts/force_control.py
@@ -5,19 +5,25 @@ import tf.transformations as tfs
 
 from geometry_msgs.msg import Vector3Stamped, WrenchStamped
 from sensor_msgs.msg import JointState
-
-from mm_kinematics import ThingKinematics
-import mm_force_control.util as util
-from mm_force_control import PID
 from mm_msgs.msg import ForceControlState
+
+import mm_kinematics.kinematics as kinematics
+
+import mm_force_control.util as util
+from mm_force_control.filter import ExponentialSmoother
+from mm_force_control.pid import PID
+from mm_force_control.bias import FTBiasEstimator
 
 
 FORCE_THRESHOLD = 5  # Force required for force control to be used
 CONTACT_FORCE = 5  # Desired contact force, when contact is detected
 MAX_INPUT_FORCE = 10  # Maximum force value that can input to the PID controller
 
-DT = 0.01
+HZ = 100  # Control loop rate (Hz)
 
+N_BIAS = 100  # Number of samples to use for force bias estimation.
+
+# Controller gains
 Kp = np.zeros(3)
 Kd = np.zeros(3)
 Ki = 0.02 * np.zeros(3)
@@ -30,71 +36,19 @@ f_T_e = tfs.translation_matrix([0.02, 0, 0]).dot(tfs.euler_matrix(-np.pi/2, 0, -
 e_T_f = tfs.inverse_matrix(f_T_e)
 
 
-def apply_transform(T, v):
-    ''' Apply 4x4 homogeneous transform T to 3d vector v. '''
-    return T.dot(np.append(v, 1))[:3]
-
-
-class FTBiasEstimator(object):
-    def __init__(self, N):
-        self.N = N
-        self.count = 0
-        self.bias = np.zeros(6)
-        self.sum = np.zeros(6)
-
-        self.ft_sub = rospy.Subscriber('/robotiq_force_torque_wrench',
-                                       WrenchStamped, self.ft_cb)
-
-    def ft_cb(self, msg):
-        if self.count < self.N:
-            data = np.array([msg.wrench.force.x, msg.wrench.force.y,
-                             msg.wrench.force.z, msg.wrench.torque.x,
-                             msg.wrench.torque.y, msg.wrench.torque.z])
-            self.sum += data
-            self.count += 1
-        else:
-            # once we have enough data to calculate the bias, we can
-            # unsubscribe from the sensor
-            self.ft_sub.unregister()
-
-    def listen(self, dt=0.1):
-        while self.count < self.N and not rospy.is_shutdown():
-            rospy.sleep(dt)
-        self.bias = self.sum / self.N
-
-    def unbias(self, force, torque):
-        f = force - self.bias[:3]
-        t = torque - self.bias[3:]
-        return f, t
-
-
-class ExponentialSmoother(object):
-    def __init__(self, tau, x0):
-        self.tau = tau
-        self.prev = x0
-
-    def next(self, measured, dt):
-        c = 1.0 - np.exp(-dt / self.tau)
-        state = c * measured + (1 - c) * self.prev
-        self.prev = state
-        return state
-
-
 class ForceControlNode(object):
     def __init__(self):
         self.pid = PID(Kp, Ki, Kd, set_point=np.zeros(3))
-        self.bias = FTBiasEstimator(100)
+        self.bias = FTBiasEstimator(N_BIAS)
 
-        # TODO filter tuning
         self.smoother = ExponentialSmoother(tau=0.1, x0=np.zeros(3))
-        self.kin = ThingKinematics()
 
         self.force_raw = np.zeros(3)
         self.force = np.zeros(3)
         self.q = np.zeros(9)
         self.time_prev = rospy.Time.now().to_sec()
 
-        # NOTE initialize subs/pubs last, since callbacks are multithreaded and
+        # subs/pubs are initialized last since callbacks are multithreaded and
         # can actually be called before other variables in __init__ have been
         # declared
         self.joint_states_sub = rospy.Subscriber('/mm_joint_states',
@@ -154,17 +108,18 @@ class ForceControlNode(object):
         msg.vector.z = p_off[2]
         self.p_off_pub.publish(msg)
 
-    def loop(self, dt):
+    def loop(self, hz):
         rospy.loginfo('Force control started. Estimating FT sensor bias...')
         self.bias.listen()
         rospy.loginfo('Estimated FT bias = {}. Control loop started.'.format(self.bias.bias))
 
+        rate = rospy.Rate(hz)
+
         while not rospy.is_shutdown():
             # Transform force to the world frame
-            # w_T_e = self.kin.calc_fk(self.q)
-            w_T_e = np.eye(4)
+            w_T_e = kinematics.forward(self.q)
             w_T_f = w_T_e.dot(e_T_f)
-            f = apply_transform(w_T_f, self.force_filt)
+            f = util.apply_transform(w_T_f, self.force_filt)
 
             # Force control. Steer toward CONTACT_FORCE if FORCE_THRESHOLD is
             # exceeded.
@@ -181,11 +136,11 @@ class ForceControlNode(object):
             self.publish_position_offset(now, p_off)
             self.publish_state(now, self.force_raw, self.force_filt, f, p_off)
 
-            # rospy.sleep(dt)
+            rate.sleep()
 
 
 if __name__ == '__main__':
     rospy.init_node('mm_force_control_node')
 
     node = ForceControlNode()
-    node.loop(DT)
+    node.loop(HZ)
