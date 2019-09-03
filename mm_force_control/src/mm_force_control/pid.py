c7c2804b7b3220d7079814ae00ee37f194eed95b
Adam Heins <mail@adamheins.com>
2019-09-03 14:47:55 -0400

    Clean up PID implementation a bit.
    * Variable renaming
    * More options are configurable via the API
    * Remove reverse output---this can be handled by the caller

--- mm_force_control/src/mm_force_control/pid.py
+++ mm_force_control/src/mm_force_control/pid.py
@@ -5,33 +5,37 @@ from mm_force_control.util import bound_array
 
 class PID(object):
     ''' PID controller for force. '''
-    def __init__(self, Kp, Ki, Kd, set_point=0, reverse_output=False):
+    def __init__(self, Kp, Ki, Kd, desired=0, decay=0, guard=1000):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
 
         self.I_term = 0
 
-        self.set_point = set_point
-        self.reverse_output = reverse_output
+        self.desired = desired
         self.last_time = rospy.get_time()
         self.last_error = 0
 
         # saturate the I term at this value
         # use to limit the amount by which the EE is allowed to deviate from
         # the reference trajectory due to force perturbation
-        self.windup_guard = 1000 * np.ones(3)  # 50
+        self.windup_guard = guard * np.ones(3)  # 50
 
         # exponential decay on the I term: I = (1-decay)*I
         # we need this if we want to return to the reference trajectory after
         # force perturbation
-        self.decay = 0  # 0.1
+        self.decay = decay  # 0.1
 
-    def update(self, feedback, set_point=None):
-        if set_point is not None:
-            self.set_point = set_point
+    def tick(self):
+        ''' Reset recorded time of previous update. Useful for manipulating
+            time after initialization but before the control loop has started. '''
+        self.last_time = rospy.get_time()
 
-        error = self.set_point - feedback
+    def update(self, actual, desired=None):
+        if desired is not None:
+            self.desired = desired
+
+        error = self.desired - actual
 
         current_time = rospy.get_time()
         delta_time = current_time - self.last_time
@@ -58,9 +62,5 @@ class PID(object):
 
         # decay on the I term
         self.I_term -= self.decay * self.I_term
-
-        # TODO why do we have this?
-        if self.reverse_output:
-            output = -output
 
         return output
