4be11e7bf607520a8572d48470a673ed1dc775af
Adam Heins <mail@adamheins.com>
2019-09-06 14:17:55 -0400

    Clean up kinematics scripts.

--- mm_kinematics/scripts/maximize_mi.py
+++ mm_kinematics/scripts/maximize_mi.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python2
 import numpy as np
 from scipy import optimize
+import matplotlib.pyplot as plt
 import mm_kinematics.kinematics as kinematics
 import IPython
 
 # Find maximum manipulability index of the Thing.
 
 q0 = np.random.random(9) * 2 * np.pi - np.pi
-N = 1000
+N = 10000
 
-def func(q):
+
+def objective(q):
     return -kinematics.manipulability(q)
 
-res = optimize.minimize(func, q0)
+
+res = optimize.minimize(objective, q0)
 m_opt = kinematics.manipulability(res.x)
 
 ms = np.zeros(N)
@@ -23,3 +26,8 @@ for i in xrange(N):
 
 print('Optimized MI = {}'.format(m_opt))
 print('Max MI after {} trials = {}'.format(N, np.max(ms)))
+
+plt.hist(ms, 100)
+plt.xlabel('Manipulability')
+plt.ylabel('Count')
+plt.show()
