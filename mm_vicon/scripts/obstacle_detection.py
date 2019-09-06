f6f9c693e2ec3fd9c0af8bd674e65db3490324f1
Adam Heins <mail@adamheins.com>
2019-09-06 14:11:16 -0400

    Refine obstacle detection.
    * Maintain ordering of obstacles (by x-coordinate).
    * Allow definition of squares in which obstacles should be included or
      excluded.

--- mm_vicon/scripts/obstacle_detection.py
+++ mm_vicon/scripts/obstacle_detection.py
@@ -10,9 +10,34 @@ R = 0.3
 HZ = 10
 
 
+class Square(object):
+    def __init__(self, c, r):
+        ''' Initialize with centre c and radius (half side length) r. '''
+        self.c = c
+        self.r = r
+
+    def contains(self, p):
+        ''' Returns True if point p is contained within the square, False
+            otherwise. '''
+        if p[0] > self.c[0] + self.r:
+            return False
+        elif p[0] < self.c[0] - self.r:
+            return False
+        elif p[1] > self.c[1] + self.r:
+            return False
+        elif p[1] < self.c[1] - self.r:
+            return False
+        return True
+
+
+INCLUDES = [Square([0, 0], 4)]
+EXCLUDES = []
+
+
 class ObstacleDetectionNode(object):
     ''' Detect non-constellation vicon markers and republish as obstacles. '''
     def __init__(self):
+        self.obstacles = []
         self.marker_sub = rospy.Subscriber('/vicon/markers', Markers,
                                            self.marker_cb)
         self.obstacle_pub = rospy.Publisher('/obstacles', Obstacles,
@@ -22,26 +47,46 @@ class ObstacleDetectionNode(object):
         # Only take unnamed markers (i.e. not part of the Thing constellation)
         markers = filter(lambda marker: marker.marker_name == '', msg.markers)
 
-        msg = Obstacles()
+        new_obstacles = []
 
         for marker in markers:
             obstacle = Obstacle()
             obstacle.radius = R
 
             # Markers have units in millimetres: convert to metres.
-            obstacle.centre.x = 0.001 * marker.translation.x
-            obstacle.centre.y = 0.001 * marker.translation.y
-            obstacle.centre.z = 0.001 * marker.translation.z
+            x = 0.001 * marker.translation.x
+            y = 0.001 * marker.translation.y
+            z = 0.001 * marker.translation.z
 
-            msg.obstacles.append(obstacle)
+            # Obstacle must be within all include squares but not in any
+            # exclude squares.
+            for square in INCLUDES:
+                if not square.contains([x, y]):
+                    continue
+            for square in EXCLUDES:
+                if square.contains([x, y]):
+                    continue
 
-        msg.header.stamp = rospy.Time.now()
-        self.obstacle_pub.publish(msg)
+            obstacle.centre.x = x
+            obstacle.centre.y = y
+            obstacle.centre.z = z
+
+            new_obstacles.append(obstacle)
+
+        # Sort obstacles so that order remains stable between Vicon frames (not
+        # required for the controller but nice for viewing). Sort is ascending
+        # in x coordinate.
+        self.obstacles = sorted(new_obstacles, key=lambda o: o.centre.x)
 
     def loop(self, hz):
-        # Just spin since we do everything in the callback
         rate = rospy.Rate(hz)
+
         while not rospy.is_shutdown():
+            msg = Obstacles()
+            msg.obstacles = self.obstacles
+            msg.header.stamp = rospy.Time.now()
+            self.obstacle_pub.publish(msg)
+
             rate.sleep()
 
 
