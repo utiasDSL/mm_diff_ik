6755f537cce0a05f7e7d932188b609dd698c5ba7
Adam Heins <mail@adamheins.com>
2019-09-10 08:31:42 -0400

    Work on trajectory visualization script.

--- mm_trajectories/scripts/visualize.py
+++ mm_trajectories/scripts/visualize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from mpl_toolkits.mplot3d import Axes3D
 
 import mm_trajectories.trajectory as trajectory
@@ -19,7 +20,7 @@ def plot_traj(traj, ax):
     ys = [wp.pose.position.y for wp in waypoints]
     zs = [wp.pose.position.z for wp in waypoints]
 
-    ax.plot(xs, ys, zs=zs)
+    ax.plot(xs, ys, zs=zs, linewidth=3)
 
 
 def main():
@@ -31,8 +32,12 @@ def main():
     square = trajectory.SquareTrajectory(np.zeros(3), quat0, DURATION)
     spiral = trajectory.SpiralTrajectory(np.zeros(3), quat0, DURATION)
 
-    fig = plt.figure()
+    # rc('text', usetex=True)
+
+    fig = plt.figure(dpi=40)
     ax = fig.add_subplot(111, projection='3d')
+
+    ax.zaxis.labelpad = 40
 
     plot_traj(line, ax)
     plot_traj(sine, ax)
@@ -40,9 +45,34 @@ def main():
     plot_traj(square, ax)
     plot_traj(spiral, ax)
 
-    ax.set_xlim([-0.1, 3.1])
+    # ax.set_title('End Effector Trajectories')
+
+    ax.set_xlim([-0, 3])
     ax.set_ylim([-1.0, 1.0])
     ax.set_zlim([-0.5, 0.5])
+
+    ax.set_xticks([0, 1, 2, 3])
+    ax.set_yticks([-1, 0, 1])
+    ax.set_zticks([-0.5, 0, 0.5])
+
+    ax.set_xlabel('\nx [m]', fontweight='bold')
+    ax.set_ylabel('\ny [m]', fontweight='bold')
+    ax.set_zlabel('\nz [m]', fontweight='bold', linespacing=7)
+
+    # set background colors to white
+    ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+    ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+    ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+
+    ax.xaxis.set_rotate_label(False)
+    ax.yaxis.set_rotate_label(False)
+    ax.zaxis.set_rotate_label(False)
+
+    ax.view_init(elev=37, azim=-135)
+
+    ax.w_xaxis.set_label_coords(0, 0)
+
+    fig.savefig('trajectories.pdf', bbox_inches='tight', pad_inches=0.2)
 
     plt.show()
 
