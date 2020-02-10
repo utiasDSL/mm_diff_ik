c5fa8d10dd2328c23311482506443e5d9ecfd01f
Adam Heins <mail@adamheins.com>
2020-02-10 14:39:29 -0500

    Add gripper control script.

---
+++ mm_force_control/scripts/gripper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+import sys
+
+import rospy
+from robotiq_s_model_control.msg import SModel_robot_output
+
+
+if __name__ == '__main__':
+    open_ = len(sys.argv) > 1 and sys.argv[1][0] == 'o'
+
+    rospy.init_node('mm_gripper_node')
+    pub = rospy.Publisher('SModelRobotOutput', SModel_robot_output, queue_size=10)
+
+    rospy.sleep(1.0)
+
+    msg = SModel_robot_output()
+    msg.rACT = 1
+    msg.rMOD = 0
+    msg.rGTO = 1
+    msg.rATR = 0
+    msg.rICF = 0
+    if open_:
+        msg.rPRA = 0
+    else:
+        msg.rPRA = 255
+    msg.rSPA = 255
+    msg.rFRA = 100
+
+    pub.publish(msg)
+
+    rospy.sleep(1.0)
