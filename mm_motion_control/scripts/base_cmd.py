acc8c60be6d0bd803cdff2fc6a46f9c9095a8a7b
Adam Heins <mail@adamheins.com>
2020-10-13 16:30:47 -0400

    Add script to send open-loop commands to base joints.

---
+++ mm_motion_control/scripts/base_cmd.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+HZ = 10
+DURATION = 10
+
+
+def main():
+    rospy.init_node('base_cmd_node')
+    cmd_pub = rospy.Publisher('/ridgeback_velocity_controller/cmd_vel', Twist, queue_size=10)
+
+    rate = rospy.Rate(10)
+
+    # velocities
+    vx = 0
+    vy = 0
+    wz = 0
+
+    # accelerations
+    ax = 0
+    ay = 0
+    az = 0
+
+    t0 = rospy.Time.now().to_sec()
+    t = t0
+    while not rospy.is_shutdown() and t - t0 < DURATION:
+        # send the command
+        msg = Twist()
+        msg.linear.x = vx
+        msg.linear.y = vy
+        msg.angular.z = wz
+        cmd_pub.publish(msg)
+
+        rate.sleep()
+
+        # update time
+        now = rospy.Time.now().to_sec()
+        dt = now - t
+        t = now
+
+        # integrate acceleration
+        vx += ax * dt
+        vy += ay * dt
+        wz += az * dt
+
+
+if __name__ == '__main__':
+    main()
