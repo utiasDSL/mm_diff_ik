9d969384e1ccc74a25ae22bbbe0f2905a52f14b1
Adam Heins <mail@adamheins.com>
2020-10-14 12:00:39 -0400

    Add optional delay to gripper script.
    Makes single-person usage easier, since one can activate gripper
    open/close and have time to go position oneself at the gripper.

--- mm_gripper/scripts/gripper.py
+++ mm_gripper/scripts/gripper.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
+# usage:
+#  rosrun mm_gripper gripper.py o|c [delay]
 import sys
 
 import rospy
@@ -8,10 +10,15 @@ from robotiq_s_model_control.msg import SModel_robot_output
 if __name__ == '__main__':
     open_ = len(sys.argv) > 1 and sys.argv[1][0] == 'o'
 
+    if len(sys.argv) > 2:
+        delay = float(sys.argv[2])
+    else:
+        delay = 0
+
     rospy.init_node('mm_gripper_node')
     pub = rospy.Publisher('SModelRobotOutput', SModel_robot_output, queue_size=10)
 
-    rospy.sleep(1.0)
+    rospy.sleep(1.0 + delay)
 
     msg = SModel_robot_output()
     msg.rACT = 1
