Fix Jacobian implementation.
It no longer relies on differentiation, either.

mm_kinematics/scripts/interact.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python2
-"""Save lambdified symbolic kinematic functions for fast loading later."""
-import os
-import sys
-import dill
-from mm_kinematics import SymbolicKinematicModel, codegen
+import numpy as np
+from mm_kinematics import SymbolicKinematicModel
 
 import IPython
 
 
 def main():
+    np.set_printoptions(precision=3, suppress=True)
+    q = np.array(
+        [
+            0,
+            0,
+            0,
+            0.0,
+            -0.75 * np.pi,
+            -0.5 * np.pi,
+            -0.25 * np.pi,
+            -0.5 * np.pi,
+            0.5 * np.pi,
+        ]
+    )
     model = SymbolicKinematicModel()
+    J = model.jacobian(q)
 
     IPython.embed()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
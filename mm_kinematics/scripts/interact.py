a094bf1b85606db735b3d971abd86508816291bc
Adam Heins <mail@adamheins.com>
2021-10-08 18:15:32 -0400

    Fix Jacobian implementation.
    It no longer relies on differentiation, either.

--- mm_kinematics/scripts/interact.py
+++ mm_kinematics/scripts/interact.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python2
-"""Save lambdified symbolic kinematic functions for fast loading later."""
-import os
-import sys
-import dill
-from mm_kinematics import SymbolicKinematicModel, codegen
+import numpy as np
+from mm_kinematics import SymbolicKinematicModel
 
 import IPython
 
 
 def main():
+    np.set_printoptions(precision=3, suppress=True)
+    q = np.array(
+        [
+            0,
+            0,
+            0,
+            0.0,
+            -0.75 * np.pi,
+            -0.5 * np.pi,
+            -0.25 * np.pi,
+            -0.5 * np.pi,
+            0.5 * np.pi,
+        ]
+    )
     model = SymbolicKinematicModel()
+    J = model.jacobian(q)
 
     IPython.embed()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- mm_kinematics/src/mm_kinematics/symbolic.py
+++ mm_kinematics/src/mm_kinematics/symbolic.py
@@ -6,33 +6,47 @@ code, and generates the differential kinematics (i.e., Jacobians) for C++.
 """
 import numpy as np
 import sympy as sym
+import sympy.vector as symv
 
 from mm_kinematics.util import R_t_from_T
 
 
-JOINT_NAMES = ['xb', 'yb', 'tb', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6']
+JOINT_NAMES = ["xb", "yb", "tb", "q1", "q2", "q3", "q4", "q5", "q6"]
 
 PARAM_SUB_DICT = {
-    'px': 0.27,
-    'py': 0.01,
-    'pz': 0.653,
-    'd1': 0.1273,
-    'a2': -0.612,
-    'a3': -0.5723,
-    'd4': 0.163941,
-    'd5': 0.1157,
-    'd6': 0.0922,
-    'd7': 0.290
+    "px": 0.27,
+    "py": 0.01,
+    "pz": 0.653,
+    "d1": 0.1273,
+    "a2": -0.612,
+    "a3": -0.5723,
+    "d4": 0.163941,
+    "d5": 0.1157,
+    "d6": 0.0922,
+    "d7": 0.290,
 }
 
 
 def symbolic_dh_transform(q, a, d, alpha):
     """Constuct a symbolic transformation matrix from D-H parameters."""
-    return sym.Matrix([
-        [sym.cos(q), -sym.sin(q)*sym.cos(alpha),  sym.sin(q)*sym.sin(alpha), a*sym.cos(q)],
-        [sym.sin(q),  sym.cos(q)*sym.cos(alpha), -sym.cos(q)*sym.sin(alpha), a*sym.sin(q)],
-        [0,           sym.sin(alpha),             sym.cos(alpha),            d],
-        [0,           0,                          0,                         1]])
+    return sym.Matrix(
+        [
+            [
+                sym.cos(q),
+                -sym.sin(q) * sym.cos(alpha),
+                sym.sin(q) * sym.sin(alpha),
+                a * sym.cos(q),
+            ],
+            [
+                sym.sin(q),
+                sym.cos(q) * sym.cos(alpha),
+                -sym.cos(q) * sym.sin(alpha),
+                a * sym.sin(q),
+            ],
+            [0, sym.sin(alpha), sym.cos(alpha), d],
+            [0, 0, 0, 1],
+        ]
+    )
 
 
 def symbolic_transform(C=sym.Matrix.eye(3), r=sym.Matrix.zeros(3, 1)):
@@ -55,6 +69,7 @@ T_ee_ft = symbolic_transform(r=sym.Matrix([0.02, 0, 0]))
 
 class SymbolicKinematicModel(object):
     """Symbolic kinematic model for the Thing mobile manipulator."""
+
     def __init__(self):
         self._calc_forward_transforms()
         self._calc_geometric_jacobian()
@@ -67,94 +82,89 @@ class SymbolicKinematicModel(object):
         self.q = sym.symbols(JOINT_NAMES)
 
         # Offset from base to arm.
-        px, py, pz = sym.symbols('px,py,pz')
+        px, py, pz = sym.symbols("px,py,pz")
 
         # Arm D-H parameters.
-        d1, a2, a3, d4, d5, d6, d7 = sym.symbols('d1,a2,a3,d4,d5,d6,d7')
+        d1, a2, a3, d4, d5, d6, d7 = sym.symbols("d1,a2,a3,d4,d5,d6,d7")
 
         self.T = [None] * 13
 
         # Transform from base to world.
-        self.T[0] = symbolic_dh_transform(sym.pi/2,  0, 0,         sym.pi/2)
-        self.T[1] = symbolic_dh_transform(sym.pi/2,  0, self.q[0], sym.pi/2)
-        self.T[2] = symbolic_dh_transform(sym.pi/2,  0, self.q[1], sym.pi/2)
-        self.T[3] = symbolic_dh_transform(self.q[2], 0, 0,         0)
+        self.T[0] = symbolic_dh_transform(sym.pi / 2, 0, 0, sym.pi / 2)
+        self.T[1] = symbolic_dh_transform(sym.pi / 2, 0, self.q[0], sym.pi / 2)
+        self.T[2] = symbolic_dh_transform(sym.pi / 2, 0, self.q[1], sym.pi / 2)
+        self.T[3] = symbolic_dh_transform(self.q[2], 0, 0, 0)
 
         # Transform from arm to base.
-        self.T[4] = symbolic_dh_transform(0, px, pz, -sym.pi/2)
-        self.T[5] = symbolic_dh_transform(0, 0,  py,  sym.pi/2)
+        self.T[4] = symbolic_dh_transform(0, px, pz, -sym.pi / 2)
+        self.T[5] = symbolic_dh_transform(0, 0, py, sym.pi / 2)
 
         # Transform from end effector to arm.
-        self.T[6]  = symbolic_dh_transform(self.q[3], 0,  d1,  sym.pi/2)
-        self.T[7]  = symbolic_dh_transform(self.q[4], a2, 0,   0)
-        self.T[8]  = symbolic_dh_transform(self.q[5], a3, 0,   0)
-        self.T[9]  = symbolic_dh_transform(self.q[6], 0,  d4,  sym.pi/2)
-        self.T[10] = symbolic_dh_transform(self.q[7], 0,  d5, -sym.pi/2)
-        self.T[11] = symbolic_dh_transform(self.q[8], 0,  d6,  0)
-        self.T[12] = symbolic_dh_transform(0,         0,  d7,  0)
+        self.T[6] = symbolic_dh_transform(self.q[3], 0, d1, sym.pi / 2)
+        self.T[7] = symbolic_dh_transform(self.q[4], a2, 0, 0)
+        self.T[8] = symbolic_dh_transform(self.q[5], a3, 0, 0)
+        self.T[9] = symbolic_dh_transform(self.q[6], 0, d4, sym.pi / 2)
+        self.T[10] = symbolic_dh_transform(self.q[7], 0, d5, -sym.pi / 2)
+        self.T[11] = symbolic_dh_transform(self.q[8], 0, d6, 0)
+        self.T[12] = symbolic_dh_transform(0, 0, d7, 0)
 
         # Transforms from intermediate points to world.
-        self.T0 = [sym.eye(4)]
+        self.T0 = [self.T[0]]
 
         # Construct intermediate transforms.
-        for i in range(13):
-            self.T0.append(self.T0[i] * self.T[i])
-
-        # self.T0[1]  = self.T0[0]  * self.T[0]
-        # self.T0[2]  = self.T0[1]  * self.T[1]   # xb
-        # self.T0[3]  = self.T0[2]  * self.T[2]   # yb
-        # self.T0[4]  = self.T0[3]  * self.T[3]   # tb - this is w_T_b
-        # self.T0[5]  = self.T0[4]  * self.T[4]
-        # self.T0[6]  = self.T0[5]  * self.T[5]
-        # self.T0[7]  = self.T0[6]  * self.T[6]   # q1
-        # self.T0[8]  = self.T0[7]  * self.T[7]   # q2
-        # self.T0[9]  = self.T0[8]  * self.T[8]   # q3
-        # self.T0[10] = self.T0[9]  * self.T[9]   # q4
-        # self.T0[11] = self.T0[10] * self.T[10]  # q5
-        # self.T0[12] = self.T0[11] * self.T[11]  # q6
-        # self.T0[13] = self.T0[12] * self.T[12]  # tool offset
+        for i in range(1, 13):
+            self.T0.append(self.T0[i - 1] * self.T[i])
 
     def _calc_geometric_jacobian(self):
         # TODO refactor this...
-        R0_0,  t0_0  = R_t_from_T(self.T0[0])
-        R0_1,  t0_1  = R_t_from_T(self.T0[1])
-        R0_2,  t0_2  = R_t_from_T(self.T0[2])
-        R0_3,  t0_3  = R_t_from_T(self.T0[3])
-        R0_4,  t0_4  = R_t_from_T(self.T0[4])
-        R0_5,  t0_5  = R_t_from_T(self.T0[5])
-        R0_6,  t0_6  = R_t_from_T(self.T0[6])
-        R0_7,  t0_7  = R_t_from_T(self.T0[7])
-        R0_8,  t0_8  = R_t_from_T(self.T0[8])
-        R0_9,  t0_9  = R_t_from_T(self.T0[9])
-        R0_10, t0_10 = R_t_from_T(self.T0[10])
-        R0_11, t0_11 = R_t_from_T(self.T0[11])
-        R0_12, t0_12 = R_t_from_T(self.T0[12])
-        R0_13, t0_13 = R_t_from_T(self.T0[13])
+        R0, t0 = R_t_from_T(self.T0[0])
+        R1, t1 = R_t_from_T(self.T0[1])
+        R2, t2 = R_t_from_T(self.T0[2])
+        R5, t5 = R_t_from_T(self.T0[5])
+        R6, t6 = R_t_from_T(self.T0[6])
+        R7, t7 = R_t_from_T(self.T0[7])
+        R8, t8 = R_t_from_T(self.T0[8])
+        R9, t9 = R_t_from_T(self.T0[9])
+        R10, t10 = R_t_from_T(self.T0[10])
+        _, p_ee = R_t_from_T(self.T0[-1])
 
         # Angular derivatives - this is the unit vector pointing along the
         # z-axis for the corresponding joint.
-        z0 = sym.Matrix([0, 0, 1])  # Unit vector along z-axis
+        z = sym.Matrix([0, 0, 1])  # Unit vector along z-axis
 
         # axis for each joint's angular velocity is the z-axis of the previous
         # transform
-        z_tb = R0_3 * z0
-        z_q1 = R0_6 * z0
-        z_q2 = R0_7 * z0
-        z_q3 = R0_8 * z0
-        z_q4 = R0_9 * z0
-        z_q5 = R0_10 * z0
-        z_q6 = R0_11 * z0
+        # TODO this may be wrong (see implementation for OCS2 controller)
+        z0 = R0 * z
+        z1 = R1 * z
+        z2 = R2 * z
+        z5 = R5 * z
+        z6 = R6 * z
+        z7 = R7 * z
+        z8 = R8 * z
+        z9 = R9 * z
+        z10 = R10 * z
 
         # joints xb and yb are prismatic, and so cause no angular velocity.
-        Jw = sym.Matrix.hstack(sym.zeros(3, 2), z_tb, z_q1, z_q2, z_q3, z_q4,
-                               z_q5, z_q6)
+        Jo = sym.Matrix.hstack(sym.zeros(3, 2), z2, z5, z6, z7, z8, z9, z10)
+        Jp = sym.Matrix.hstack(
+            z0,
+            z1,
+            z2.cross(p_ee - t2),
+            z5.cross(p_ee - t5),
+            z6.cross(p_ee - t6),
+            z7.cross(p_ee - t7),
+            z8.cross(p_ee - t8),
+            z9.cross(p_ee - t9),
+            z10.cross(p_ee - t10)
+        )
 
         # Linear derivatives
-        Jvs = [sym.diff(t0_13, qi) for qi in self.q]
-        Jv = sym.Matrix.hstack(*Jvs)
+        # Jps = [sym.diff(t0_13, qi) for qi in self.q]
+        # Jp = sym.Matrix.hstack(*Jvs)
 
         # Full Jacobian
-        self.J = sym.Matrix.vstack(Jv, Jw)
+        self.J = sym.Matrix.vstack(Jp, Jo)
 
     def _lambdify_functions(self):
         """Create lamdified kinematic functions."""
